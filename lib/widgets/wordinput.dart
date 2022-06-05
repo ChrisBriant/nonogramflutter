b3900e15478298674e4d42e25e937d477063a7de
Your Name <cbriant@btinternet.com>
2022-06-05 16:43:08 +0100

    Added a feature to disable the score button if it is still processing the entered words.

--- lib/widgets/wordinput.dart
+++ lib/widgets/wordinput.dart
@@ -19,6 +19,7 @@ class _WordInputState extends State<WordInput> {
   String? _wordsString;
   Timer? _timer;
   String tempWord = '';
+  bool processingWordList = true;
 
   //Define regex expressions
   final Map<int,RegExp> regExMap = {
@@ -31,16 +32,16 @@ class _WordInputState extends State<WordInput> {
     9 : RegExp(r'\b\w{9}\b',multiLine: true),
   };
 
-  _saveForm() {
-    print('Form saves here');
-    print(_wordsString);
+  // _saveForm() {
+  //   print('Form saves here');
+  //   print(_wordsString);
 
-    _formKey.currentState!.save();
-    if(_nonogramProvider != null) {
-      //_nonogramProvider!.sendWords(_wordsString);
-    }
+  //   _formKey.currentState!.save();
+  //   if(_nonogramProvider != null) {
+  //     //_nonogramProvider!.sendWords(_wordsString);
+  //   }
   
-  }
+  // }
 
 
 bool _checkWordInWord(String word1, String word2) {
@@ -49,100 +50,66 @@ bool _checkWordInWord(String word1, String word2) {
   }
   String newWord2 = word2;
   String newWord1 = word1;
-  // print(newWord2);
   String letter = word1.characters.take(1).toString();
   int letterIndex = newWord2.characters.toList().indexWhere((element) => element == letter);
-  // print('letter index $letterIndex');
-  // print(newWord2);
   if(letterIndex == -1) {
-    // print('word not valid');
     return false;
   } else {
-    //newWord2 = newWord2.characters.toList().removeAt(letterIndex);
-    // print('Word list');
     List<String> newWord2List = newWord2.characters.toList();
     newWord2List.removeAt(letterIndex);
     List<String> newWord1List = newWord1.characters.toList();
     newWord1List.removeAt(0);
     //Need to conver the array to string
-    // print('Word valid ${newWord2List.join('')}');
     return _checkWordInWord(newWord1List.join(''), newWord2List.join(''));
-    //return true;
   }
 }
 
-
-
   //Performs word matching
   _matchWords(wordText) {
-    print('three seconds passed');
-    print(wordText);
     String testStr = '3';
     String nonogramWord = _nonogramProvider!.nonogram.word;
 
 
     for(int i=3;i<10;i++) {
-      //final String regExStr = '\b\\w{$i}\b';
       final RegExp? regEx = regExMap[i];
-      print('$i letter matches');
       regEx!.allMatches(wordText.toLowerCase()).forEach((word) { 
-        bool validWord = true; 
-        // word.group(0).toString().characters.forEach((letter) {  
-        //   if(!nonogramWord.contains(letter)) {
-        //     print('$letter NOT IN WORD');
-        //     validWord = false;
-        //   }
-        // });
         if(_checkWordInWord(word.group(0).toString(), nonogramWord)) {
           //Add the word
-          print(word.group(0));
           _nonogramProvider!.addNLetterWord(i,word.group(0).toString());
-        } else {
-          print('INVALID ${word.group(0)}');
         }
-
       });
     }
   }
 
   _readWords(val) {
-    print('Reading words here');
     //print(_inputContoller.text);
+    setState(() {
+      processingWordList =  true;
+    });
     if(_timer != null) {
       _timer!.cancel();
       _timer = Timer(Duration(seconds: 3), () {
           _matchWords(val);
+          setState(() {
+            processingWordList = false;
+          });
+          
         }
       );
     } else {
       _timer = Timer(Duration(seconds: 3), () {
           _matchWords(val);
+          setState(() {
+            processingWordList = false;
+          });
         }
       );
     }
-
-    // _timer = Timer(Duration(seconds: 3), () {
-    //     print('three seconds passed');
-    //     print(val);
-    //   }
-    // );
-
-
-    // _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-    //   print('one second passed');
-    //   print(val);
-    // });
-    // for(int i=3;i<10;i++) {
-    //   //final String regExStr = '\b\\w{$i}\b';
-    //   final regEx = RegExp(r'\b\w{' + i.toString() + '}\b',multiLine: true);
-    //   print(regEx.allMatches(val));
-    // }
     
   }
 
   _submitWords() async {
     try {
-      print('Trying to use this.');
       bool success = await _nonogramProvider!.sendWords();
       if(success) {
         Navigator.of(context).pushNamed(ResultsScreen.routeName);
@@ -152,11 +119,9 @@ bool _checkWordInWord(String word1, String word2) {
         ));
       }
     } catch (err) {
-      print('I am handling the exception');
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
         content: Text(err.toString()),
       ));
-      print(err.toString());
     }
   }
 
@@ -175,12 +140,11 @@ bool _checkWordInWord(String word1, String word2) {
                     onChanged: (value) { _readWords(value); },
                     decoration: const InputDecoration(labelText: 'Enter words below:'),
                     keyboardType: TextInputType.text,
-                    //validator: (value) => Validators.validText(value),
                     onSaved: (value) { _wordsString = value; },
                   ),
         ),
         ElevatedButton(
-          onPressed: _submitWords, 
+          onPressed: processingWordList ? null : _submitWords, 
           child: const Text('Score')
         )
       ],
